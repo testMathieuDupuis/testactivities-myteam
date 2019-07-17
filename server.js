2697ab8d26e9623f877b684babd243fc3edf4c68
Ibrahim Bchouti <ibchouti19@gmail.com>
2019-07-17 14:55:14 -0400

    Added saving session to dynamodb

--- server.js
+++ server.js
@@ -6,6 +6,18 @@ const serverConfig = require('./config/config')
 
 const app = express()
 
+const session = require('express-session');
+const DynamoStore = require('connect-dynamodb-session')(session);
+app.use(session({
+    cookie: { maxAge: 1209600000 },
+    secret: 'PFE2019_RATIO',
+    store: new DynamoStore({
+        region: 'ca-central-1',
+        tableName: 'ratio_session',
+        autoCreate: true
+    })
+}));
+
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
